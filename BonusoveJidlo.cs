f23131c847b1801dd11aa4ff00e84374434d38e7
xuananh0 <thanhniennghiem98@gmail.com>
2021-01-18 14:28:16 +0100

    Bonusove jidlo

---
+++ BonusoveJidlo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Had
+{
+    class BonusoveJidlo : Jidlo
+    {
+        public BonusoveJidlo(int x, int y) : base(x, y)
+        {
+        }
+
+        public override void VykresliSe(Graphics g)
+        {
+            g.FillEllipse(Brushes.BlueViolet, pozice.X * velikost, pozice.Y * velikost, velikost, velikost);
+        }
+    }
+}
